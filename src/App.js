0edb92cf0bd7c57f7893aa4e395ee4b55df4749c
Sefa BAŞAR <info@sefabasar.com.tr>
2020-03-18 19:34:45 +0300

    yazı değiştirildi

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sefa's First React App
+          Git Öğrenme Commiti
         </p>
         <a
           className="App-link"
